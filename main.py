31f598c67370dd07f54ff9a0fbaa8e28eee468e9
terapotan <rs582855@pc4.so-net.ne.jp>
2019-05-18 10:38:31 +0900

    テスト自動認識仮実装

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import tests.numtest
 # クラス名を忘れがち。大抵ファイル名とクラス名は一緒にするので余計分かりにくい。
 # 結構悩んだ。pythonのimportってめんどくさい。
 
-
-
-
-
-
-
 class mainTest(unittest.TestCase):
     def test_calltests(self):
         inst = tests.numtest.numtest()
-        self.assertEqual(inst.retNum,1)
+        self.assertEqual(inst.retNum(),1)
+    def test_canTestsInTestsFolderCall(self):
+        # NLUnitで実行されるテストは全てtestsフォルダに入れられる
+        # 1つのファイルにつき1つのテスト実行用クラスが入る
+        # クラスの中に存在するメソッド1つ1つが1つのテストケースに相当する
+        # テストケースは自動認識(実行するテストケースをユーザー側が指定する必要はない)されるようにする
+        self.assertEqual(TestsInTestsFolderCall(),0)
+                
+# TODO:例外処理は後で実装
+# TODO:後で自動認識機能を実装
+def TestsInTestsFolderCall():
+    return 0
 
 
+
+
+
+
+# ほかのファイルからimportされたときにテストが実行されないようにする
 if __name__ == "__main__":
     unittest.main()
