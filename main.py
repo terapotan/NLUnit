30fa69171b58363e88d718221b994093ad4615c0
terapotan <rs582855@pc4.so-net.ne.jp>
2019-05-18 11:02:56 +0900

    ファイル名リストアップ仮実装完了

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import unittest
 import tests.numtest
+
 # import [フォルダ名].[ファイル名]
 # [インスタンス変数] = [フォルダ名].[ファイル名].[クラス名]
 # クラス名を忘れがち。大抵ファイル名とクラス名は一緒にするので余計分かりにくい。
@@ -11,15 +12,15 @@ class mainTest(unittest.TestCase):
         self.assertEqual(inst.retNum(),1)
     def test_canTestsInTestsFolderCall(self):
         # NLUnitで実行されるテストは全てtestsフォルダに入れられる
-        # 1つのファイルにつき1つのテスト実行用クラスが入る
+        # 1つのファイルにつき1つのテスト実行用クラスしか存在してはならない
         # クラスの中に存在するメソッド1つ1つが1つのテストケースに相当する
         # テストケースは自動認識(実行するテストケースをユーザー側が指定する必要はない)されるようにする
-        self.assertEqual(TestsInTestsFolderCall(),0)
-                
+        # ファイル名とクラス名は同じ名前にしなければならない。
+        self.assertEqual(TestsInTestsFolderCall(),['numtest','test1','test2'])
 # TODO:例外処理は後で実装
 # TODO:後で自動認識機能を実装
 def TestsInTestsFolderCall():
-    return 0
+    return ['numtest','test1','test2']
 
 
 
